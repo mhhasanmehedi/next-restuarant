357a95ff6b0087f207b10d9a1d2cb73797e7fe52
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-17 18:03:02 +0600

    Product listing

---
+++ components/PizzaList.jsx
@@ -0,0 +1,28 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. ut blandit arcu
+        in prelim molestic.Interdum et maesuda fames acme. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit
+      </p>
+
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
