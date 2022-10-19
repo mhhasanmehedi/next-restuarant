3e1a6a347ca8ef2aad8eed669d3e56e9679eb681
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-19 09:13:51 +0600

    Nextjs Rest api with mongodb done

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/PizzaCard.module.css";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
+  const { _id, title, desc, img, prices } = pizza;
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="Pizza" width="500" height="500" />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-      <span className={styles.price}>$19.99</span>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit.
-      </p>
+      <Link href={`/product/${_id}`}>
+        <Image src={img} alt="Pizza" width="500" height="500" />
+      </Link>
+      <h1 className={styles.title}>{title}</h1>
+      <span className={styles.price}>${prices[0]}</span>
+      <p className={styles.desc}>{desc}</p>
     </div>
   );
 };
